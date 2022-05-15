76bc671506a39778f728de9c4f8809ddf94c0c7c
Ashish Patel <ashishpatel626@lavabit.com>
2022-05-14 23:19:38 -0400

    Initial commit

---
+++ Class_Objects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+{
+    public class Person{
+        public string Name = "";
+        public int Age;
+        public bool HasPet;
+
+        public int Calc(int x, int y){
+            int number = x + y;
+            return number;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person sam = new Person();
+            sam.Name = "sam";
+            sam.Age = 3;
+            sam.HasPet = true;
+            
+            int t = sam.Calc(3, 4);
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program{
+    class test{
+        static void Main(string[] args){
+            string[] cars;
+            cars = new string[] {"honda"};
+
+            Console.WriteLine(cars[0]);
+            
+            for (int i = 0; i < cars.Length; i++){
+                Console.WriteLine(cars[i]);
+            }
+        }
+    }
+}
